acd67d4ef080e3d685316903ba5a005da5301782
veeramani2004 <veeramanipalanisamy642004@gmail.com>
2025-08-23 16:27:11 +0530

    Python introduction

---
+++ DAY_28_PYTHON/01_start.py
@@ -0,0 +1,36 @@
+# print("hello,world")
+
+
+# name = input("Tell me Your name?:")
+# birth_year = input("Tell me your birth year?:")
+# year = 2025 - int(birth_year)
+# print("hi " + name + " you are " + str(year) + " years old"
+
+# print(f"hi,{name} you are {2025 - int(birth_year)} years old")
+
+
+# fahrenheit=input
+
+
+# radius = float(input("Provite the radius of the :"))
+# area = 3.14 * radius**2
+# print(f"Area of the circle {area}")
+
+
+# percentage = int(input("Give a number?:"))
+# bar = percentage // 10
+# progress = "=" * bar
+# space = " " * (10 - bar)
+# print(f"[{progress}{space}] {percentage}%")
+
+# fav_movie = "John  Wick"
+
+# print(fav_movie[5:8]
+
+
+quote = "Dream is not something that you see in sleep, Dream is something that does not let you sleep"
+print(quote.count("Dream"))  # 2
+
+
+print(quote.replace("Dream", "🛌💭"))
+print(quote.find("something"))
